7e0ac238a5d64cf01791af959318ce5203822f5e
Yuriko Maruyama <yuriko-maruyama@hiroshima-u.ac.jp>
2021-06-23 01:46:26 +0000

    test2

--- ex1.go
+++ ex1.go
@@ -4,19 +4,64 @@ import (
 	"fmt"
 )
 
+// #include <iostream>
+
+// using namespace std;
+
+// string introm(int a) {
+//   int intnum[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+//   const char *romnum[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
+//                           "XL", "X",  "IX", "V",  "IV", "I"};
+//   string s;
+//   int i = 0;
+//   while (a) {
+//     while (a / intnum[i]) {
+//       s += romnum[i];
+//       a -= intnum[i];
+//     }
+//     i++;
+//   }
+//   return s;
+// }
+
 func intToRome(n int) string {
 	// nは入力された数字
 
 	var rome string // ローマ数字
 	var quo int     // 商
+	//  var arr[2] string = [2]string {"Golang", "Java"}
+	// var 変数名 [長さ]型 = [大きさ]型{初期値1, 初期値n}
+	// 変数名 := [...]型{初期値１, 初期値n}
 
-	if n > 0 {
-		quo = n / 1000
+	intnum := [...]int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	romnum := [...]string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+
+	for i := 0; i < 13; i++ {
+		quo = n / intnum[i]
+		for j := quo; j > 0; j-- {
+			rome += romnum[i]
+		}
+		n = n % intnum[i]
 	}
 
-	for i := 1; i <= quo; i++ {
-		rome += "M"
-	}
+	// 	// 千の位を扱う
+	// 	if n > 0 {
+	// 		quo = n / 1000
+	// 	}
+
+	// 	for i := 1; i <= quo; i++ {
+	// 		rome += "M"
+	// 	}
+
+	// 	// 百の位を扱う
+	// 	n = n % 1000
+	// 	if n > 0 {
+	// 		quo = n / 1000
+	// 	}
+
+	// 	for i := 1; i <= quo; i++ {
+	// 		rome += "M"
+	// 	}
 
 	return rome
 
--- ex1_test.go
+++ ex1_test.go
@@ -10,3 +10,12 @@ func TestIntToRome01(t *testing.T) {
 		t.Errorf("Test01 is failed expected: %s, result: %s\n", expected, result)
 	}
 }
+
+func TestIntToRome02(t *testing.T) {
+	expected := "CM"
+	input := 900
+	result := intToRome(input)
+	if expected != result {
+		t.Errorf("Test02 is failed expected: %s, result: %s\n", expected, result)
+	}
+}
