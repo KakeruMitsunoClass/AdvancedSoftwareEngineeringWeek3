464738954b215ada4be12edc9669717b2403bced
Yuriko Maruyama <yuriko-maruyama@hiroshima-u.ac.jp>
2021-06-23 02:30:45 +0000

    ex2_test2

--- ex2/ex2_test.go
+++ ex2/ex2_test.go
@@ -10,3 +10,39 @@ func TestRomeToInt01(t *testing.T) {
 		t.Errorf("Test01 is failed expected: %d, result: %d\n", expected, result)
 	}
 }
+
+func TestRomeToInt02(t *testing.T) {
+	expected := 2008
+	input := "MMVIII"
+	result := romaToInt(input)
+	if expected != result {
+		t.Errorf("Test02 is failed expected: %d, result: %d\n", expected, result)
+	}
+}
+
+func TestRomeToInt03(t *testing.T) {
+	expected := 99
+	input := "XCIX"
+	result := romaToInt(input)
+	if expected != result {
+		t.Errorf("Test03 is failed expected: %d, result: %d\n", expected, result)
+	}
+}
+
+func TestRomeToInt04(t *testing.T) {
+	expected := 47
+	input := "XLVII"
+	result := romaToInt(input)
+	if expected != result {
+		t.Errorf("Test04 is failed expected: %d, result: %d\n", expected, result)
+	}
+}
+
+func TestRomeToInt05(t *testing.T) {
+	expected := 1990
+	input := "MCMXC"
+	result := romaToInt(input)
+	if expected != result {
+		t.Errorf("Test05 is failed expected: %d, result: %d\n", expected, result)
+	}
+}
